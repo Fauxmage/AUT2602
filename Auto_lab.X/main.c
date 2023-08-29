16b9453187c7e990d5e932142890fddaf014b4d9
Ulrik Johnsen <ulrikjohnsen@tromso-stud-3106.wifi.uit.no>
2023-08-29 14:21:59 +0200

    Finished some probles for assignmnet

--- Auto_lab.X/main.c
+++ Auto_lab.X/main.c
@@ -5,36 +5,86 @@
  * Created on August 28, 2023, 2:18 PM
  */
 
+#define F_CPU 4000000UL
+
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define BUTTON_PIN 2
-#define LED_PIN 3
 
+
+// Function for problem 2a, init the LED on PB3 by setting the state to HIGH
 void init_led() {
-    PORTB.DIRSET = (1 << LED_PIN); // Set Pin 3 of Port B as an output
+    PORTB.DIRSET = (1 << 3);
 }
 
-void init_button() {
-    PORTB.DIRCLR = (1 << BUTTON_PIN); // Set Pin 2 of Port B as an input
-    PORTB.PIN2CTRL |= PORT_PULLUPEN_bm; // Enable the pull-up resistor on Pin 2
+
+// Function for problem 2b, toggle the output with a specified delay in the main fucntion
+void led_blink(){
+    PORTB.OUTTGL = (1 << 3);
 }
+
+void init_button(){
+    PORTB.DIRCLR = (1 << 2); // Set Pin 2 of Port B as an input
+    PORTB.PIN2CTRL |= PORT_PULLUPEN_bm; // Enable the pull-up resistor on Pin 2
+
+}
+
 
 int main(void) {
     init_led();
     init_button();
     
     while (1) {
-        // Check if the button on Pin 2 is pressed
-        if (!(PORTB.IN & (1 << BUTTON_PIN))) {
-            // Button is pressed, turn on the LED
-            PORTB.OUTSET = (1 << LED_PIN); // Set Pin 3 of Port B to HIGH
-        } else {
-            // Button is not pressed, turn off the LED
-            PORTB.OUTCLR = (1 << LED_PIN); // Clear Pin 3 of Port B (set to LOW)
+
+        /*
+        // OPPGAVE 2a) 
+        init_led();
+        */
+       
+
+       /*
+       // Problem 2b) The LED on PB3 will blink with 1000ms delay
+       led_blink();
+       _delay_ms(1000);
+        */
+
+
+
+        /*Problem 2c) What is a pull-up? 
+        */
+
+
+
+       // Problem 2d) Make LED light up when button is pushed down
+       // If button is pushed down, then set PB3 to HIGH
+        if (PORTB.IN & (1 << 2)){
+            PORTB.OUTSET = (1 << 3);
         }
+
+        // If button is not pushed down, then set PB3 to LOW
+        else{
+            PORTB.OUTCLR = (1 << 3); 
+        }
+
+
+
     }
     
     return 0;
 }
 
+// // Check if the button on Pin 2 is pressed
+// if (!(PORTB.IN & (1 << BUTTON_PIN))) {
+//     // Button is pressed, turn on the LED
+//     PORTB.OUTSET = (1 << LED_PIN); // Set Pin 3 of Port B to HIGH
+// } else {
+//     // Button is not pressed, turn off the LED
+//     PORTB.OUTCLR = (1 << LED_PIN); // Clear Pin 3 of Port B (set to LOW)
+// }
+
+/*
+void init_button() {
+    PORTB.DIRCLR = (1 << 2); // Set Pin 2 of Port B as an input
+    PORTB.PIN2CTRL |= PORT_PULLUPEN_bm; // Enable the pull-up resistor on Pin 2
+}
+*/
