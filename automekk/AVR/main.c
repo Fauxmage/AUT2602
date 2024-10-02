18c6bd2bded8ee236909a78049193e4ccf7716f1
Ulrik Johnsen <ulrikjohnsen@tromso-studenter1-1637.wifi.uit.no>
2024-10-02 13:09:21 +0200

    Formatting code from shitmess clangd lsp left it in

--- automekk/AVR/main.c
+++ automekk/AVR/main.c
@@ -11,22 +11,30 @@
 
 #include "adc.h"
 #include "usart.h"
+#include "rtc.h"
 
 #define BUFF 16
 
+#define INT_CLK 0
+#define EXT_CLK 1
+
+#define RTC_ENABLE 1
+#define RTC_DISABLE 0
+
+
 int main(void) {
+	// Initialize USART3, PB, Baudrate 9600, tx-pin as outputusart_usb_init
+	usart_usb_init(9600);
+	init_temp_sensor();
+	
+	//rtc_init(RTC_ENABLE, INT_CLK);
 
-  // Initialize USART3, PB, Baudrate 9600, tx-pin as output
-  usart_usb_init(9600);
+	printf("This is a test!\n");
 
-  init_temp_sensor();
+	while (1) {
+		uint8_t temp = adc_read_temp();
+		printf("Temp: %dC\n", temp);
 
-  printf("This is a test!\n");
-
-  while (1) {
-    uint8_t temp = adc_read_temp();
-    printf("Temp: %dC\n", temp);
-
-    _delay_ms(1000);
+		_delay_ms(1000);
   }
 }
--- automekk/AVR/usart.c
+++ automekk/AVR/usart.c
@@ -11,64 +11,66 @@ char usb_message[256];
 volatile uint8_t marker_pos;
 volatile uint8_t usb_message_ready;
 
-void usart_usb_init(uint16_t baud) {
-  // Set baud rate
-  USART3.BAUD = B_RATE(baud);
-  // Enable TX and RX
-  USART3.CTRLB |= USART_TXEN_bm | USART_RXEN_bm;
-  // Set TX pin as output
-  PORTB.DIR |= (1 << TX_PIN_bp);
-  // Enable RX complete interrupt (if necessary)
-  // USART3.CTRLA |= USART_RXCIE_bm;
 
-  // Redirect stdout to the USART stream
-  stdout = &new_std_out;
+void usart_usb_init(uint16_t baud){
+	// Set baud rate
+	USART3.BAUD = B_RATE(baud);
+	// Enable TX and RX
+	USART3.CTRLB |= USART_TXEN_bm | USART_RXEN_bm;
+	// Set TX pin as output
+	PORTB.DIR |= (1 << TX_PIN_bp);
+	// Enable RX complete interrupt (if necessary)
+	// USART3.CTRLA |= USART_RXCIE_bm;
+
+	// Redirect stdout to the USART stream
+	stdout = &new_std_out;
 }
 
-void usart_usb_transmit(char c) {
-  // Wait until the transmit buffer is empty
-  while (!(USART3.STATUS & USART_DREIF_bm))
-    ;
-  // Transmit the character
-  USART3.TXDATAL = c;
+
+void usart_usb_transmit(char c){
+	// Wait until the transmit buffer is empty
+	while (!(USART3.STATUS & USART_DREIF_bm));
+	// Transmit the character
+	USART3.TXDATAL = c;
 }
 
-void usart_usb_transmit_char_array(uint8_t addEndLine, char string[],
-                                   uint8_t length) {
-  for (uint8_t i = 0; i < length; i++) {
-    usart_usb_transmit(string[i]);
-  }
-  if (addEndLine)
-    usart_usb_transmit('\n');
+
+void usart_usb_transmit_char_array(uint8_t addEndLine, char string[], uint8_t length){
+	for (uint8_t i = 0; i < length; i++){
+		usart_usb_transmit(string[i]);
+	}
+	if (addEndLine){
+		usart_usb_transmit('\n');
+	}
 }
 
-char usart_usb_receive() {
-  // Wait for a character to be received
-  while (!(USART3.STATUS & USART_RXCIF_bm))
-    ;
-  return USART3.RXDATAL;
+
+char usart_usb_receive(){
+	// Wait for a character to be received
+	while (!(USART3.STATUS & USART_RXCIF_bm));
+	return USART3.RXDATAL;
 }
 
-void usart_transmit_char(USART_t *usart_nr, char c) {
-  // Wait until the transmit buffer is empty
-  while (!(usart_nr->STATUS & USART_DREIF_bm))
-    ;
-  // Transmit the character
-  usart_nr->TXDATAL = c;
+
+void usart_transmit_char(USART_t *usart_nr, char c){
+	// Wait until the transmit buffer is empty
+	while (!(usart_nr->STATUS & USART_DREIF_bm));
+	// Transmit the character
+	usart_nr->TXDATAL = c;
 }
 
-uint8_t usart_usb_transmit_printf(char c, FILE *stream) {
-  // Wait until the transmit buffer is empty
-  while (!(USART3.STATUS & USART_DREIF_bm))
-    ;
-  // Transmit the character
-  usart_usb_transmit(c);
-  return 0;
+
+uint8_t usart_usb_transmit_printf(char c, FILE *stream){
+	// Wait until the transmit buffer is empty
+	while (!(USART3.STATUS & USART_DREIF_bm));
+	// Transmit the character
+	usart_usb_transmit(c);
+	return 0;
 }
 
-void usart_transmit_string(USART_t *usart_nr, char *string,
-                           usart_char_transmitter transmit_char) {
-  for (uint8_t i = 0; string[i] != '\0'; i++) {
-    transmit_char(usart_nr, string[i]);
-  }
+
+void usart_transmit_string(USART_t *usart_nr, char *string, usart_char_transmitter transmit_char) {
+	for (uint8_t i = 0; string[i] != '\0'; i++){
+		transmit_char(usart_nr, string[i]);
+	}
 }
